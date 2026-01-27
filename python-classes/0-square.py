e3b4a5083c277c1efba252be8d4bfcb37c8fea81
root <root@ip-10-42-154-124.ec2.internal>
2026-01-27 15:03:28 +0000

    Add Task 0: empty Square class with docstrings and

---
+++ python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module defines an empty Square class.
+The Square class will be used as a starting point
+for learning Object-Oriented Programming in Python.
+"""
+
+
+class Square:
+    """
+    This is an empty class that defines a square.
+    It does not have attributes or methods yet.
+    """
+    pass
