d3fbf30d36071b2b572e5608dd6200229a125646
root <root@ip-10-42-154-124.ec2.internal>
2026-01-27 16:14:29 +0000

    Task 0 rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module defines an empty Rectangle class.
+The Rectangle class will be expanded in later tasks.
+"""
+
+
+class Rectangle:
+    """
+    This is an empty class that defines a rectangle.
+    It does not have attributes or methods yet.
+    """
+    pass
