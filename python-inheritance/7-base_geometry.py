ca32bced1eae3ce417733d4d18ce33bbfe0c887f
root <root@ip-10-42-186-29.ec2.internal>
2026-02-03 17:35:36 +0000

    Fix BaseGeometry with proper integer_validator and doctest

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -19,14 +19,16 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """
         Validates that value is a positive integer.
+
         Args:
             name (str): name of the variable
             value (int): value to validate
+
         Raises:
             TypeError: if value is not an integer
             ValueError: if value <= 0
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
