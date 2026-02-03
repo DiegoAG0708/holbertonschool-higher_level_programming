4a9a72787cad762a11a25563301ba3ead56bafa0
root <root@ip-10-42-186-29.ec2.internal>
2026-02-03 17:15:31 +0000

    Add empty BaseGeometry class

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 5-base_geometry
+Defines an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class intended to be a base for geometry objects.
+    """
+    pass
