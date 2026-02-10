587934d2064e7d55a2754971cb24ae6339912b17
root <root@ip-10-42-42-170.ec2.internal>
2026-02-10 15:35:56 +0000

    Implement append_write function to append UTF8 text and return character count

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module that provides the append_write function.
+It appends a string to a UTF-8 text file and returns
+the number of characters added.
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends a string at the end of a text file (UTF8)
+    and returns the number of characters added.
+
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append.
+
+    Returns:
+        int: The number of characters added.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
