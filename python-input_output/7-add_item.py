78d05eba8e4cf947db5191d9ccc23e9842a00fc4
root <root@ip-10-42-42-170.ec2.internal>
2026-02-10 17:07:22 +0000

    Implement 7-add_item.py script to load, add, and save arguments into add_item.json

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-"""
-Script that adds all arguments to a Python list
-and saves them to a file as JSON.
-"""
+"""Adds command-line arguments to a list and saves them to a JSON file."""
 
 import sys
-import os
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 
 filename = "add_item.json"
 
-# Load existing list if file exists, otherwise start with empty list
-if os.path.exists(filename):
+try:
     items = load_from_json_file(filename)
-else:
+except Exception:
     items = []
 
-# Extend list with command-line arguments
 items.extend(sys.argv[1:])
-
-# Save updated list back to file
 save_to_json_file(items, filename)
