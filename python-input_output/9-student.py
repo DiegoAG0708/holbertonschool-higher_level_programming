f2fdd74ddc6190220428cf57799b8dd62d8e69a2
root <root@ip-10-42-42-170.ec2.internal>
2026-02-10 17:15:54 +0000

    Implement Student class with to_json method for dictionary representation

---
+++ python-input_output/9-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module that defines the Student class.
+"""
+
+
+class Student:
+    """
+    Class that defines a student by first_name, last_name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance.
+
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: The dictionary representation of the Student.
+        """
+        return self.__dict__
