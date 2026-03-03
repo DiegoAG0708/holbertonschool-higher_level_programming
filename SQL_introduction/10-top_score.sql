1fdd79f848e2addc0e06cf68f852934993bf0b65
root <root@ip-10-42-82-150.ec2.internal>
2026-03-03 16:14:07 +0000

    Task 10: Add script to list records from second_table ordered by score

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- Task 10: List records ordered by score
+-- This script selects score and name from second_table, ordered by score descending
+SELECT score, name FROM second_table ORDER BY score DESC;
