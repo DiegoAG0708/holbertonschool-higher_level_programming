9336ea91ce4f5760065a73387076c7e0e39feb0e
root <root@ip-10-42-82-150.ec2.internal>
2026-03-03 17:20:01 +0000

    Task 10: Add script to list shows with their genre IDs

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- Task 10: Genre ID by show
+-- This script lists all shows with at least one genre linked
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows, tv_show_genres
+WHERE tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
