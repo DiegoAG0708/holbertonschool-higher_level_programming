35369c23b2795373a5f0635236eea29da89d47d0
root <root@ip-10-42-61-133.ec2.internal>
2026-03-03 19:00:47 +0000

    Task 0: Add script to list all states from hbtn_0e_0_usa

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,12 +7,10 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Get MySQL credentials and database name from arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -21,16 +19,11 @@ if __name__ == "__main__":
         db=database
     )
 
-    # Create cursor
     cur = db.cursor()
-
-    # Execute query: select all states ordered by id
     cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Fetch and print results
     for row in cur.fetchall():
         print(row)
 
-    # Close cursor and connection
     cur.close()
     db.close()
