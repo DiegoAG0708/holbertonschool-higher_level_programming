9dc349fd3d8de814fed197346c1ff60d590de564
root <root@ip-10-42-82-150.ec2.internal>
2026-03-03 17:06:36 +0000

    Task 5: Add script to create unique_id table with unique id defaulting to 1

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,7 @@
+-- Task 5: Unique ID
+-- This script creates the table unique_id with id defaulting to 1 and unique
+
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
