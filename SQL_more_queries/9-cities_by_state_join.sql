0bf7dcb81eb9fde53268f2deeccff2bd7f01a8af
root <root@ip-10-42-82-150.ec2.internal>
2026-03-03 17:18:10 +0000

    Task 9: Add script to list all cities with their states ordered by city id

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Task 9: Cities by States
+-- This script lists all cities with their state names, ordered by city id
+
+SELECT cities.id, cities.name, states.name
+FROM cities, states
+WHERE cities.state_id = states.id
+ORDER BY cities.id ASC;
