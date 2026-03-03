d36cc84c60092c6b1d3985fd4afe3048602186c6
root <root@ip-10-42-82-150.ec2.internal>
2026-03-03 15:11:24 +0000

    Task 4: Add script to create first_table with id and name columns

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Task 4: Create first_table
+-- This script creates a table named first_table with columns id and name
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
