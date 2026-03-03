0b5f4e69f74c561f35d3b36615d458ae945334ac
root <root@ip-10-42-82-150.ec2.internal>
2026-03-03 16:45:59 +0000

    Task 3: Add script to create force_name table with NOT NULL name

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+-- Task 3: Always a name
+-- This script creates the table force_name with id and name (name cannot be NULL)
+
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
